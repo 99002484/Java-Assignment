98e638ff0ff11e273c9e408d4821973ecb999a96
99002484 <71481506+99002484@users.noreply.github.com>
2020-10-20 09:15:08 +0530

    Create package.java

---
+++ package.java
@@ -0,0 +1,29 @@
+package com.collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Emp employee1 = new Emp("Pradeep","Mysore",122);
+		Emp employee2 = new Emp("Rama","Banglore",124);
+		Emp employee3 = new Emp("Ravi","Manglore",125);
+		Emp employee4 = new Emp("Raman","Mandya",126);
+		ArrayList<Employee> employeeList = new ArrayList<>();
+		employeeList.add(employee1);
+		employeeList.add(employee2);
+		employeeList.add(employee3);
+		employeeList.add(employee4);
+		for(Emp emp : employeeList) {
+			System.out.println(emp);
+		}
+		Collections.sort(employeeList);
+		System.out.println("Sorted List");
+		for(Emp emp : employeeList) {
+			System.out.println(emp);
+		}
+		
+	}
+}
