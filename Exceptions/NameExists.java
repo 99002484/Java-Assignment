ddf1d054bcecb04a83e093cd6a42657ab85cf4ee
99002484 <71481506+99002484@users.noreply.github.com>
2020-10-20 09:26:45 +0530

    Create NameExists.java

---
+++ Exceptions/NameExists.java
@@ -0,0 +1,21 @@
+package com.Exceptions;
+
+public class NameEException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NameEException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public NameEException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
